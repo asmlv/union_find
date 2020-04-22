635d3accd1a1eb40a269deefd9bfdb5688cbcc67
asmlv <asamoilov@keemail.me>
2020-04-22 17:10:31 +0300

    Недокоммичено

--- src/Program.cs
+++ src/Program.cs
@@ -5,28 +5,10 @@ using System.Linq;
 namespace UnionFind {
 
     class Program {
-        public const int EXPERIMENT_COUNT = 1;
+        public const int EXPERIMENT_COUNT = 10;
 
         static void Main(string[] args) {
-            var qu = new QuickUnionUF(5);
-            qu.Open(20);
-            qu.Open(9);
-            qu.Open(2);
-            qu.Open(13);
-            qu.Open(19);
-            qu.Open(21);
-            qu.Open(24);
-            qu.Open(5);
-            qu.Open(17);
-            qu.Open(8);
-            qu.Open(22);
-            qu.Open(12);
-            qu.Open(4);
-            qu.PrintField();
-        //    qu.PrintPercolatedField();
-        /*
-            //var N = int.Parse(args[0]);            
-            var N = 5;
+            var N = int.Parse(args[0]);            
             var results = new List<double>(EXPERIMENT_COUNT);
             for (var i = 0; i < EXPERIMENT_COUNT; i++) { 
                 var qu = new QuickUnionUF(N);
@@ -40,7 +22,7 @@ namespace UnionFind {
                     }
                 }       
             }           
-            Console.WriteLine("Percolation threshold = " + results.Average()); */
+            Console.WriteLine("Percolation threshold = " + results.Average());
         }
     }
 }
